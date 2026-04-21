b1e971250b747e3bd7f18a1ef8fbb699f6d8800e
Sukhdeep <singhkaursukhdeep@fpllefia.com>
2026-04-21 13:24:49 +0200

    Afegir funcionalitat per carregar i mostrar cartes de Clash Royale des de Supabase a index.js i actualitzar index.html

--- cartasroyale/index.js
+++ cartasroyale/index.js
@@ -1,4 +1,30 @@
+// Dades del teu projecte Supabase (les trobes a Settings → API)
+let supabaseUrl = "https://lvlcgvbwaegbxebkmxyc.supabase.co/rest/v1/";   // La URL del projecte
+let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2bGNndmJ3YWVnYnhlYmtteHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTY4OTUsImV4cCI6MjA5MjMzMjg5NX0.pJejiJ_d7wvnjyxrndVxqeDSqPcUzmHba4kLPl7_1Tg";  // La clau "anon public"
+
+// Creem el client: és l'objecte que ens permet llegir i escriure a la base de dades
+let client = supabase.createClient(supabaseUrl, supabaseAnonKey);
+
 console.log("Hola cartas")
+
+// Funció que demana a Supabase totes les notícies i les pinta a la pàgina
+async function carregarCartas() {
+  // await = espera a tenir les dades abans de continuar; select("*") = agafa totes les columnes
+  let resultat = await client.from("cartas").select("*");
+  // resultat.data és l'array de notícies; si hi ha error, posem array buit
+  arrayCartas = resultat.data;
+  // Passem l'array a la funció que genera l'HTML i el posa al div #llistaNoticies
+  renderitzarCartas(noticies);
+}
+carregarCartas()
+function renderitzarCartas(){
+    for(let i = 0; i <arrayCartas.length;i++){
+    console.log(arrayCartas[i].id,arrayCartas[i].nombre)
+    divCartas = divCartas + `<div class="cartas border-1 w-[213px] h-[255px] rounded-4xl shadow-lg"><img src="${arrayCartas[i].imagen}" alt=""></div>`
+    }
+    console.log(divCartas)
+    document.querySelector("#container").innerHTML = divCartas
+}
 let divCartas = ""
 const arrayCartas = [
   {
@@ -30,3 +56,4 @@ for(let i = 0; i <arrayCartas.length;i++){
 }
 console.log(divCartas)
 document.querySelector("#container").innerHTML = divCartas
+
